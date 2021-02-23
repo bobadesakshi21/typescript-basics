7005e32691dc890b125514c5c7e008d4576f8281
Sakshi Bobade <bobadesakshi21@gmail.com>
2021-02-23 07:21:33 +0530

    Configuring Typescript

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
+"use strict";
 var num1Element = document.getElementById('num1');
 var num2Element = document.getElementById('num2');
+// const buttonElement = document.querySelector('button')
 var buttonElement = document.querySelector('button');
 function add(num1, num2) {
     return num1 + num2;
 }
+// if (buttonElement) {
 buttonElement.addEventListener('click', function () {
     var num1 = num1Element.value;
     var num2 = num2Element.value;
     var result = add(+num1, +num2);
     console.log("RESULT: ", result);
 });
+// }
 console.log(add(1, 2));
 // console.log(add('1', '2'))
